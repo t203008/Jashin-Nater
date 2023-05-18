51e95202d3c6465f7f541d6cb9a1ac56b8e7738e
t203008 <91816208+t203008@users.noreply.github.com>
2023-05-19 00:06:44 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import numpy as np 
+import pandas as pd
+import base64
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from PIL import Image
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
 st.title("ジャシネーター")
 st.write("あなたが考えている邪神ちゃんのキャラを当てるんですの。")
 st.write("※マイナー過ぎるキャラクターは出てこない可能性があるんですの")
+
+data=pd.read_csv("Jashin-Chan")
